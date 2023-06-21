54f760353f35cd427542e816dd3c9527d4b79705
jojupiter <sedoufofe@hmail.com>
2023-06-21 04:12:23 -0400

    add no dim

--- result/views.py
+++ result/views.py
@@ -21,30 +21,56 @@ class ResultAPIView(APIView):
     def post(self, request):
         text = request.data.get('text')
         #text= self.translate_text('en_US',text)
-        text = self.clean_text(text)
         print("text",text)
         results = self.detectspam(text)
         serializer1 = ResultSerializer(results[0])
         serializer2 = ResultSerializer(results[1])
         serializer3 = ResultSerializer(results[2])
-        return Response([text,serializer1.data,serializer2.data,serializer3.data])
+        serializer4 = ResultSerializer(results[3])
+        serializer5 = ResultSerializer(results[4])
+        serializer6 = ResultSerializer(results[5])
+        serializer7 = ResultSerializer(results[6])
+
+        return Response([text,serializer1.data,serializer2.data,serializer3.data,serializer4.data,serializer5.data,serializer6.data,serializer7.data])
     
 
     def detectspam(self, text):
         model1 = joblib.load('modele1.pkl')   # charger le modele
         model2 = joblib.load('modele2.pkl')   # charger le modele
         model3 = joblib.load('modele3.pkl')   # charger le modele
-        X_test= self.preprocess(text)                    # pretraiter le mail
+        rf_model = joblib.load('rf_model.pkl')   # charger le modele
+        nb_model = joblib.load('nb_model.pkl')   # charger le modele
+        logreg_model = joblib.load('logreg_model.pkl')   # charger le modele
+        svm_model = joblib.load('svm_model.pkl')   # charger le modele
+        # pretraiter le mail
+        X_test= self.preprocess(text)
         prediction1 = model1.predict(X_test)
         prediction2 = model2.predict(X_test)
         prediction3 = model3.predict(X_test)
+        # pretraiter le mail avec des modeles sans reductions de dimensions
+        X_test_no_dim = self.preprocess_no_dim(text)
+        prediction4 = rf_model.predict(X_test_no_dim)
+        prediction5 = nb_model.predict(X_test_no_dim)
+        prediction6 = logreg_model.predict(X_test_no_dim)
+        prediction7 = svm_model.predict(X_test_no_dim)
+
         print ('prediction1', prediction1)
         print ('prediction2', prediction2)
         print ('prediction3', prediction3)
+        #tester les mails avec les modeles sans reductions de dimention
+        print('prediction4', prediction4)
+        print('prediction5', prediction5)
+        print('prediction6', prediction6)
+        print('prediction7', prediction7)
+
         message = Message.objects.create(message_text=text)
         algo1 = Algo.objects.create(name="Regression Logistique")
         algo2 = Algo.objects.create(name="Forets Aleatoire")
         algo3 = Algo.objects.create(name="SVM(Support Vector Machine)")
+        algo4 = Algo.objects.create(name="Forests Aleatoires")
+        algo5 = Algo.objects.create(name="Naifs Bayes")
+        algo6 = Algo.objects.create(name="regression logistique")
+        algo7 = Algo.objects.create(name="SVM(Support Vector Machine)")
         result1 = Result.objects.create(
             accuracy=0.97757847,
             precision=0.9489051,
@@ -72,10 +98,47 @@ class ResultAPIView(APIView):
             message=message,  # Remplacez "message_instance" par l'instance appropriée de la classe Message
             algo=algo3  # Remplacez "algo_instance" par l'instance appropriée de la classe Algo
         )
-        results =[result1,result2,result3]
+        result4 = Result.objects.create(
+            accuracy=0.977578475,
+            precision=0.992063492,
+            recall=0.8389261744,
+            f1=0.9090909,
+            spam=self.getSpam(prediction4[0]),
+            message=message,  # Remplacez "message_instance" par l'instance appropriée de la classe Message
+            algo=algo4  # Remplacez "algo_instance" par l'instance appropriée de la classe Algo
+        )
+        result5 = Result.objects.create(
+            accuracy=0.977578475,
+            precision=0.992063492,
+            recall=0.8389261744,
+            f1=0.9090909,
+            spam=self.getSpam(prediction5[0]),
+            message=message,  # Remplacez "message_instance" par l'instance appropriée de la classe Message
+            algo=algo5  # Remplacez "algo_instance" par l'instance appropriée de la classe Algo
+        )
+        result6 = Result.objects.create(
+            accuracy=0.977578475,
+            precision=0.992063492,
+            recall=0.8389261744,
+            f1=0.9090909,
+            spam=self.getSpam(prediction6[0]),
+            message=message,  # Remplacez "message_instance" par l'instance appropriée de la classe Message
+            algo=algo6  # Remplacez "algo_instance" par l'instance appropriée de la classe Algo
+        )
+        result7 = Result.objects.create(
+            accuracy=0.977578475,
+            precision=0.992063492,
+            recall=0.8389261744,
+            f1=0.9090909,
+            spam=self.getSpam(prediction7[0]),
+            message=message,  # Remplacez "message_instance" par l'instance appropriée de la classe Message
+            algo=algo7  # Remplacez "algo_instance" par l'instance appropriée de la classe Algo
+        )
+        results =[result1,result2,result3,result4,result5,result6,result7]
         return results
     
     def preprocess(self, text):
+        text = self.clean_text(text)
         vectorizer = CountVectorizer()
         # Importer le vocabulaire du vectoriseur
         vocab = joblib.load('vectorizer_vocab.pkl')
@@ -125,3 +188,16 @@ class ResultAPIView(APIView):
         print("Detected source language: {}".format(result["detectedSourceLanguage"]))
 
         return result["translatedText"]
+
+    def preprocess_no_dim(self,text):
+        vectorizer = CountVectorizer()
+        # Importer le vocabulaire du vectoriseur
+        vocab = joblib.load('vectorizer_vocab_no_dim.pkl')
+        # Charger les paramètres du vectoriseur
+        vectorizer_params = joblib.load('vectorizer_params_no_dim.pkl')
+        vectorizer.set_params(**vectorizer_params)
+        vectorizer.vocabulary_ = vocab
+        X_test = vectorizer.transform([text])
+        print("X shape no dim", X_test.shape)
+        print("X test no dim", X_test)
+        return  X_test
